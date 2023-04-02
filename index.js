6010355cdb57e8b35124fb9ca60b7a284153a982
kennethcasey88 <kennethcasey88@gmail.com>
2023-04-02 10:22:02 +0200

    updated the functions to work properly

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
 let randoVal = Math.floor(Math.random() * 3) + 1;
+let cpu;
+let winner;
+let loser;
+getComputerChoice();
 function getComputerChoice() {
     if (randoVal === 2) {
-        let cpu = "paper";
+        cpu = "paper";
         return cpu;
     } else if (randoVal === 3) {
-        let cpu = "scissors";
+        cpu = "scissors";
         return cpu;
     } else  {
-       let cpu = "rock";
+       cpu = "rock";
        return cpu;
     }
 }
 let cpuChoice = getComputerChoice();
 let player = prompt("Rock, Paper or Scissors?");
+
 let lower = player.toLowerCase();
-function playRound(lower, cpuChoice) {
-    let lose1 = ((lower === "rock") && (cpuChoice === "paper"));
-    let lose2 = ((lower === "scissors") && (cpuChoice === "rock"));
-    let lose3 = ((lower === "paper") && (cpuChoice === "scissors"));
-    let draw = (lower === cpuChoice);
-    let loser = 1;
-    let winner = 1;
-    if (lose1) {
-        console.log("You lose! Paper beats Rock");
+console.log(lower);
+    console.log(cpuChoice);
+playRound(lower, cpuChoice);
+function playRound(player, cpu) {
+    console.log(player);
+    console.log(getComputerChoice());
+    if ((player === "rock") && (cpu === "paper")) {
+        alert("You lose! Paper beats Rock");
         return loser;
-    } else if (lose2) {
-        console.log("You lose! Rock beats Scissors");
+    } else if ((player === "scissors") && (cpu === "rock")) {
+        alert("You lose! Rock beats Scissors");
         return loser;
-    } else if (lose3) {
-        console.log("You lose! Scissors beats Paper");
+    } else if ((player === "paper") && (cpu === "scissors")) {
+        alert("You lose! Scissors beats Paper");
         return loser;
-    } else if (draw) {
-        console.log("Tie game");
-    } else {
-        console.log("You win!");
+    } else if (player === cpu) {
+        alert("Tie game");
+    }     else {
+        alert("You win!");
         return winner;
     }
 }
-console.log(playRound(lower, cpuChoice));
