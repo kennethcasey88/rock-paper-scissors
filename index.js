3c4665df0ae36ef36d3867f21ecd22ca94b1f743
kennethcasey88 <kennethcasey88@gmail.com>
2023-04-16 16:20:24 +0200

    Finished the game and everything seems to function properly

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ let i;
 let playerChoice;
 let results = 0;
 let final = 0;
+let userScore = 0;
+let cpuScore = 0;
+const rockBtn = document.querySelector('#rockBtn');
+const paperBtn = document.querySelector('#paperBtn');
+const scisBtn = document.querySelector('#scisBtn');
+const user = document.querySelector('#user');
+const computer = document.querySelector('#computer');
+const message = document.querySelector('#message');
+
+rockBtn.addEventListener('click', () => rounds('rock', cpuFunc()));
+paperBtn.addEventListener('click', () => rounds('paper', cpuFunc()));
+scisBtn.addEventListener('click', () => rounds('scissors', cpuFunc()));
+
 function cpuFunc(cpuChoice) {
     let randoVal = Math.floor(Math.random() * 3) + 1;
     if (randoVal === 1) {
@@ -15,42 +28,84 @@ function cpuFunc(cpuChoice) {
         return cpuChoice;
     }
 }
-function game() {
-for (i = 0; i < 5; i++) {
-playerChoice = prompt("Game begins!").toLowerCase();
-let cpuRoll = cpuFunc();
-cpuFunc();
-results = rounds(playerChoice, cpuRoll);
 
-final += results;
-
-}
-if (final > 0) {
-    console.log("You win");
-} else if (final < 0) {
-    console.log("You suck loser") 
-} else {
-    console.log("Tie game")
-}
-}
 
 function rounds(player, cpu) {
-    let result;
+    let result = 0;
     if ((player === "rock") && (cpu === "paper")) {
-        alert("You lose! Paper beats Rock");
-        return result = -1;
+        
+        result = -1;
+        scoreKeeper(result);
     } else if ((player === "scissors") && (cpu === "rock")) {
-        alert("You lose! Rock beats Scissors");
-        return result = -1;
+        
+        result = -1;
+        scoreKeeper(result);
     } else if ((player === "paper") && (cpu === "scissors")) {
-        alert("You lose! Scissors beats Paper");
-        return result = -1;
+        
+        result = -1;
+        scoreKeeper(result);
     } else if (player === cpu) {
-        alert("Tie game");
-        return result = 0;
+        
+        result = 0;
+        scoreKeeper(result);
     }     else {
-        alert("You win!");
-        return result = 1;
+        
+        result = 1;
+        scoreKeeper(result);
     }
 }
-game();
+
+function scoreKeeper(data) {
+        if (data < 0) {
+            computer.textContent = ++cpuScore;
+            document.getElementById("message").style.color = "red";
+            message.textContent = "YOU LOSE THE ROUND LOL!";
+        } else if (data > 0) {
+            user.textContent = ++userScore;
+            document.getElementById("message").style.color = "green";
+            message.textContent = "YOU WIN THE ROUND!";
+        } else {
+            document.getElementById("message").style.color = "yellow";
+            message.textContent = "DRAW!";
+        }
+
+        if (cpuScore == 5) {
+            document.getElementById("message").style.color = "red";
+            message.textContent = "GAME OVER LOSER HAHAHAHA!";
+            userScore = 0;
+            cpuScore = 0;
+            user.textContent = userScore;
+            computer.textContent = cpuScore;
+        } else if (userScore == 5) {
+            document.getElementById("message").style.color = "green";
+            message.textContent = "YOU WIN THE GAME" ;
+            userScore = 0;
+            cpuScore = 0;
+            user.textContent = userScore;
+            computer.textContent = cpuScore;
+        }
+}
+
+
+
+
+
+
+function game() {
+    for (i = 0; i < 5; i++) {
+    playerChoice = prompt("Game begins!").toLowerCase();
+    let cpuRoll = cpuFunc();
+    cpuFunc();
+    results = rounds(playerChoice, cpuRoll);
+    
+    final += results;
+    
+    }
+    if (final > 0) {
+        console.log("You win");
+    } else if (final < 0) {
+        console.log("You suck loser") 
+    } else {
+        console.log("Tie game")
+    }
+    }
